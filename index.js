c88e156cc461987f2ac6564585abc478dbc96f84
Datta2305 <sridattajwalachakravarthi@gmail.com>
2025-08-10 16:03:15 +0530

    Backend

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const vendorRoutes = require('./routes/vendorRoutes');
+const bodyparser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+const PORT = 4000;
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
+
+app.use(bodyparser.json());
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes);
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+app.use('/home', (req, res)=>{
+    res.send("<h1> Welcome to SUBY")
+})
+// app.use('/vendor/register', (req, res)=>{
+//     res.send("<h1> Welcome to register")
+// })
